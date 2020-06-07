8e0f925e7bf22451d8d6c60c481444d54813b075
shimo0108 <syk8153181531@gmail.com>
2020-06-07 17:27:27 +0900

    背景画像挿入 レイアウト調整

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -20,13 +20,11 @@ class RoomsController < ApplicationController
   end
 
   def index
-    # ログインユーザーが属しているルームのIDを全て抽出して配列化
     current_entries = current_user.entries
     my_room_ids = []
     current_entries.each do |entry|
       my_room_ids << entry.room.id
     end
-    # さらにuser_idがログインユーザーでは無いレコードを抽出
     @another_entries = Entry.where(room_id: my_room_ids).where.not(user_id: current_user.id)
   end
 end
