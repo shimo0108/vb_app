de429a3d2ea2baa5b0d0823d130d8c6259d47344
shimo0108 <syk8153181531@gmail.com>
2020-06-12 16:55:54 +0900

    usershowのコード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,21 +7,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    currentUserEntry = current_user.entries
-    userEntry = Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      currentUserEntry.each do |cu|
-        userEntry.each do |u|
-          if cu.room_id == u.room_id
-            @isRoom = true
-            @roomId = cu.room_id
-          end
-        end
-      end
-      unless @isRoom
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
+    currentUserEntry = current_user.entries.pluck(:room_id)
+    userEntry = @user.entries.pluck(:room_id)
+    common_room_ids = currentUserEntry & userEntry
+    return if @user.id == current_user.id
+    return if @roomId = common_room_ids.select.first
+    @room = Room.new
+    @entry = Entry.new
   end
 end
