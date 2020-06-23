a90808ed6cb4efdedaa940fe19a8d34f3335076d
shimo0108 <syk8153181531@gmail.com>
2020-06-23 09:59:50 +0900

    player_positions追加修正

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,13 +12,12 @@ class PlayersController < ApplicationController
   def create
     @player = current_user.build_player(player_params)
       if @player.save
-        positions_id = params[:position_id]
-          positions_id.each do |category|
-            PlayerPosition.create(player_id: @player.id, position_id: category)
+        positions_id = params[:positions]
+          positions_id.each do |position_id|
+            PlayerPosition.create(player_id: @player.id, position_id: position_id )
           end
         flash[:success] = "登録が完了しました。"
         redirect_to root_path
-        binding.pry
       else
         flash[:danger] = "登録できませんでした"
         render 'new'
