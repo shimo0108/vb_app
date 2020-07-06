3002e29cf36553315cafe62d06cbabcba6135eaf
shimo0108 <syk8153181531@gmail.com>
2020-07-06 10:27:25 +0900

    seedsファイル編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-
 30.times do |n|
   last_name = "太郎"
   first_name = "test#{n+1}"
@@ -18,11 +17,24 @@ User.create(last_name: "ユーザー",
             email: "test@example.com",
             password: "password"
           )
-Position.create(display_name: "ウィングスパイカー",name: "wing_spiker")
-Position.create(display_name: "ミドルブロッカー",name: "middle_brocker")
-Position.create(display_name: "セッター",name: "setter")
-Position.create(display_name: "オポジット",name: "opposite")
-Position.create(display_name: "リベロ",name: "libero")
+
+Position.create(id:1, display_name: "ウィングスパイカー",name: "wing_spiker")
+Position.create(id:2, display_name: "ミドルブロッカー",name: "middle_brocker")
+Position.create(id:3, display_name: "セッター",name: "setter")
+Position.create(id:4, display_name: "オポジット",name: "opposite")
+Position.create(id:5, display_name: "リベロ",name: "libero")
+
+users = User.order(:created_at).take(15)
+  15.times do 
+  users.each { |n| n.create_player!(gender:  Faker::Number.rand(1..2),
+                                prefecture: Faker::Number.rand(1..47),
+                                available_day: Faker::Number.rand(1..3),
+                                comment: "よろしくおねがいします！"            
+                              )}
+  end
+players = Player.all
+  players.each { |n| n.player_positions.create!(player_id:n.id,position_id: Faker::Number.rand(1..5))}
+
 
 
 
