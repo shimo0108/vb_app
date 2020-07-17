453b6c47411f11ad909b7750ace54fc95db91c4a
shimo0108 <syk8153181531@gmail.com>
2020-07-17 09:55:47 +0900

    twittercard仮作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def get_twitter_card_info()
+    twitter_card = {}
+    twitter_card[:url] = 'https://www.meiboo.net/'
+    twitter_card[:title] =　'バレーボールマッチングサイト「LETS VOLLEY BALL」'
+    twitter_card[:description] = 'バレーボールができる環境。きっと見つかります。'
+    twitter_card[:image] = '/Users/shimosasashouta/environment/vb/app/assets/images/home_images.jpeg'
+    twitter_card[:card] = 'summary'
+    twitter_card[:site] = '@pandama09396862'
+    twitter_card
+  end
 end
