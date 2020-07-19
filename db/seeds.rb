8f7e902a7f26bd2f9c8d781077e0dde97d208fec
shimo0108 <syk8153181531@gmail.com>
2020-07-19 23:46:25 +0900

    css修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,6 @@
               password: password
 )
 end
-User.create(last_name: "ユーザー",
-            first_name: "テスト",
-            nickname: "テストユーザー",
-            email: "test@example.com",
-            password: "password"
-          )
 
 Position.create(id:1, display_name: "ウィングスパイカー",name: "wing_spiker")
 Position.create(id:2, display_name: "ミドルブロッカー",name: "middle_brocker")
@@ -24,15 +18,15 @@ Position.create(id:3, display_name: "セッター",name: "setter")
 Position.create(id:4, display_name: "オポジット",name: "opposite")
 Position.create(id:5, display_name: "リベロ",name: "libero")
 
-users = User.order(:created_at).take(6)
-15.times do
-  content = Faker::Games::Fallout.quote
+users = User.order(:created_at).take(30)
+5.times do
+  content = "はじめまして！東京都でバレーができる人を募集しています。"
   users.each { |user| user.microposts.create!(content: content) }
 end
 
 
-users = User.order(:created_at).take(15)
-15.times do 
+users = User.order(:created_at).take(25)
+25.times do 
   users.each { |n| n.create_player!(gender:  Faker::Number.rand(1..2),
                                 prefecture: Faker::Number.rand(1..47),
                                 available_day: Faker::Number.rand(1..3),
@@ -42,12 +36,33 @@ end
 players = Player.all
   players.each { |n| n.player_positions.create!(player_id:n.id,position_id: Faker::Number.rand(1..5))}
 
+users = User.order(created_at: :DESC).take(25)
+25.times do 
+  users.each { |n| n.create_team!(
+                                name: Faker::Sports::Football.team,
+                                gender:  Faker::Number.rand(1..2),
+                                prefecture: Faker::Number.rand(1..47),
+                                available_day: Faker::Number.rand(1..3),
+                                comment: "よろしくおねがいします！"            
+                              )}
+end
+teams = Team.all
+  teams.each { |n| n.team_positions.create!(team_id:n.id,position_id: Faker::Number.rand(1..5))}
+
+
 users = User.all
 user  = users.second
 following = users[3..50]
 followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+User.create(last_name: "ユーザー",
+            first_name: "テスト",
+            nickname: "テストユーザー",
+            email: "test@example.com",
+            password: "password"
+          )
 
 
 
